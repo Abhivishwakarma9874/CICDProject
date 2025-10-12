4d234b57a2232ab60e11513308842e50ff352f34
Abhivishwakarma9874 <av3002563@gmail.com>
2025-10-12 21:02:00 +0530

    Set up dockerfile

--- app.py
+++ app.py
@@ -22,5 +22,8 @@ def services():
 def contact():
     return render_template("contact.html")
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
+
